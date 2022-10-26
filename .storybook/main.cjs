d4c114da90ed14119dbd21e81a40a29dccd66f76
Mairon Vilela <maironvilela@hotmail.com>
2022-10-26 20:15:45 -0300

    fix: deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -13,12 +13,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }/*,
+  },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/ignite-lab-design-systems/'
     }
 
     return config
-  }*/
+  }
 }
